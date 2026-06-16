3d9b4875467721b95856fa7d25b2104d3498e6c2
Thanh Nguyen <102184395+Wil486Thanh@users.noreply.github.com>
2026-06-16 13:10:48 -0700

    Implement main app structure with navigation

--- Lab_04/main.dart
+++ Lab_04/main.dart
@@ -1,1 +1,75 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Affirmations',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(colorSchemeSeed: Colors.blue),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  String? _selectedYear;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Main Screen")
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: const Text('Modify Data'),
+              onPressed: () async {
+                final selectedYear = await Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => ModifyDataScreen()),
+                );
+                setState(() {
+                  _selectedYear = selectedYear;
+                });
+              },
+            ),
+            SizedBox(height: 20),
+            Text('Selected Pizza: $_selectedYear')
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ModifyDataScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Modify Data")
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text("Go back"),
+        ),
+      ),
+    );
+  }
+}
