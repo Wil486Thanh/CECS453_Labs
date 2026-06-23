e3c9c556a130ea3177e2a37c6b296f1438717044
Eden <Jtmcflea@gmail.com>
2026-06-23 12:47:29 -0700

    add constructor with placeholder data to WeatherData class

--- Lab_06/main.dart
+++ Lab_06/main.dart
@@ -43,6 +43,13 @@ class WeatherData {
       lowTemp = json['main']['temp_min'],
       city = json['name'],
       windSpeed = json['wind']['speed'];
+
+  factory WeatherData.defaults() {
+    final defaultString =
+        '{"coord":{"lon":-117.9537,"lat":33.7092},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01d"}],"base":"stations","main":{"temp":77.2,"feels_like":77.49,"temp_min":73.06,"temp_max":83.35,"pressure":1013,"humidity":61,"sea_level":1013,"grnd_level":1009},"visibility":10000,"wind":{"speed":8.01,"deg":288,"gust":14},"clouds":{"all":0},"dt":1782168821,"sys":{"type":2,"id":2007369,"country":"US","sunrise":1782132120,"sunset":1782183936},"timezone":-25200,"id":5350207,"name":"Fountain Valley","cod":200}';
+    final Map<String, dynamic> json = jsonDecode(defaultString);
+    return new WeatherData.fromJson(json);
+  }
 }
 
 class WeatherDisplay extends StatelessWidget {
@@ -89,22 +96,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final jsonString =
-      '{"coord":{"lon":-117.9537,"lat":33.7092},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01d"}],"base":"stations","main":{"temp":77.2,"feels_like":77.49,"temp_min":73.06,"temp_max":83.35,"pressure":1013,"humidity":61,"sea_level":1013,"grnd_level":1009},"visibility":10000,"wind":{"speed":8.01,"deg":288,"gust":14},"clouds":{"all":0},"dt":1782168821,"sys":{"type":2,"id":2007369,"country":"US","sunrise":1782132120,"sunset":1782183936},"timezone":-25200,"id":5350207,"name":"Fountain Valley","cod":200}';
 
   @override
   Widget build(BuildContext context) {
-    final thingy = jsonDecode(jsonString) as Map<String, dynamic>;
-    print(WeatherData.fromJson(thingy));
-
     return Scaffold(
       appBar: AppBar(title: Text(widget.title)),
       body: Center(
         child: Column(
           mainAxisAlignment: .center,
-          children: [WeatherDisplay(data: .fromJson(thingy))],
+          children: [WeatherDisplay(data: WeatherData.defaults())],
         ),
       ),
     );
   }
 }
+
